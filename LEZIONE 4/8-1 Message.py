5f6290c7980e3996c9e9f3adfe761f195506d549
Gogiche9 <mirkoperone04@gmail.com>
2025-04-04 18:15:02 +0200

    Add files via upload

---
+++ LEZIONE 4/8-1 Message.py
@@ -0,0 +1,7 @@
+#8-1 Message
+
+def display_message():
+    print("I'm learning about fuctions and problem solving!")
+
+display_message()
+
---
+++ LEZIONE 4/8-2 Favorite Book.py
@@ -0,0 +1,6 @@
+#8-2 Favorite Book
+
+def favorite_book(title):
+    print(f"Il mio libro preferito è {title}")
+
+favorite_book("Alice in Wonderland")
